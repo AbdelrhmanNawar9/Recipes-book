6ecf093286b87f98792cae3a8a3d89656d197f95
AbdelrhmanNawar9 <abdonawar97@gmail.com>
2022-05-16 23:31:58 +0200

    after basics

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'this is a test',
+      'https://www.saveur.com/uploads/2022/04/30/47_Zaba_9780805243390_art_r1-scaled.jpg?auto=webp&width=1440&height=960.1875'
+    ),
+    new Recipe(
+      'A test recipe',
+      'this is a test',
+      'https://www.saveur.com/uploads/2022/04/30/47_Zaba_9780805243390_art_r1-scaled.jpg?auto=webp&width=1440&height=960.1875'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
